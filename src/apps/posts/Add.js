b8e8757a95cb087930deb0d721175990b9f18fdf
Farzin <francisdotbio@gmail.com>
2021-09-25 00:49:19 +0330

    add post

--- src/apps/posts/Add.js
+++ src/apps/posts/Add.js
@@ -1,19 +1,59 @@
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+import { useAddPostMutation } from "./_api";
+import { useState } from "react";
+
 const Add = () => {
+    const history = useHistory();
+
+    const [
+        addPost,
+        { isLoading: updating, isSuccess: saved }
+    ] = useAddPostMutation();
+
+    const [post, setpost] = useState({});
+
+    const save = (e) => {
+        e.preventDefault();
+        addPost(post)
+            .unwrap()
+            .then(() => {
+                goBack(1500);
+            });
+    };
+
+    const handler = (e) => {
+        const { name, value } = e.target;
+        setpost({ ...post, [name]: value });
+    };
+
+    const goBack = (time) => {
+        setTimeout(() => {
+            history.push("/posts/");
+        }, time);
+    };
+
     return (
-        <form className="form">
+        <form onSubmit={save} className="form">
             <h2>Add Post</h2>
 
             <label>Title</label>
-            <input name="title" type="text" />
+            <input onChange={handler} name="title" type="text" />
 
             <label>Content</label>
-            <textarea name="content" rows="10"></textarea>
+            <textarea onChange={handler} name="content" rows="10"></textarea>
 
             <footer className="form-footer">
                 <Link className="btn btn-default" to="/">Cancel</Link>
-                <button className="btn btn-primary" type="submit">Add</button>
+                <button className="btn btn-primary" type="submit">{updating ? "Adding..." : "Add"}</button>
             </footer>
+
+            {saved && (
+                <div className="alert">
+                    Post has been added. redirecting...
+                </div>
+            )}
         </form>
     )
 }
